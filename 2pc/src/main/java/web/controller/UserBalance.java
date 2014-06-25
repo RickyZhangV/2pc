2e29eb38558cd2325148f48c37a1c27f1a7fe160
RickyZhangV <zhangyidong0007@hotmail.com>
2014-06-26 02:05:29 +0800

    update

---
+++ 2pc/src/main/java/web/controller/UserBalance.java
@@ -0,0 +1,17 @@
+package web.controller;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping ( "user" )
+public class UserBalance {
+	
+	@RequestMapping("balance.do")
+	public String balance(){
+		System.out.println(123);
+		return "user/balance";
+	}
+}
--- 2pc/src/web/controller/MainConroller.java
+++
@@ -1,7 +0,0 @@
-package web.controller;
-
-public class MainConroller {
-	public static void main(String[] args) {
-		System.out.println(1);
-	}
-}
