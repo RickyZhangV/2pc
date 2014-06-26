718d98334270b82c181cbf816b6a8e8cd94e054a
RickyZhangV <zhangyidong0007@hotmail.com>
2014-06-27 02:18:50 +0800

    update

---
+++ 2pc/config-backup/sql/tbl_user_account.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.6.17-enterprise-commercial-advanced : Database - demo
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`demo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `demo`;
+
+/*Table structure for table `tbl_user_account` */
+
+DROP TABLE IF EXISTS `tbl_user_account`;
+
+CREATE TABLE `tbl_user_account` (
+  `accountId` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8_bin DEFAULT '',
+  `balance` decimal(10,2) DEFAULT '0.00',
+  PRIMARY KEY (`accountId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `tbl_user_account` */
+
+insert  into `tbl_user_account`(`accountId`,`username`,`balance`) values (1,'Peter','1000.00'),(2,'Jack','500.00'),(3,'Ricky','800.00'),(4,'Coco','400.00');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
